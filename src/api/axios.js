5cbeb174c690dba43b484541f4d6216b519579c3
Gonzalo Martinez <gonchimartinez9@gmail.com>
2026-04-10 02:52:51 -0300

    creando el axios con key

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  // Vite usa import.meta.env para leer las variables de Vercel. 
+  // Le ponemos "||" por si estás probando en tu PC y Vercel no está.
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api', 
+  
   withCredentials: true 
 });
 
